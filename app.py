e37f7ae8bbc6c166c5c4aa03a59c1b6329985bbd
Jaden Young Sang <jadenys2002gmail.com>
2023-02-12 22:15:10 -0500

    pip install complete

--- app.py
+++ app.py
@@ -2,31 +2,40 @@ from fastapi import FastAPI, Request, HTTPException
 from bson import ObjectId
 import motor.motor_asyncio
 import pydantic
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://week4:Lb30oh1KEZTEVypf@cluster0.cklfqkn.mongodb.net/?retryWrites=true&w=majority")
+origins = [
+    "http://localhost.80000",
+    "https://ecse3038-lab3-tester.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://jaden123:Jaden123@ac-3wetz71-shard-00-00.js0j7bp.mongodb.net:27017,ac-3wetz71-shard-00-01.js0j7bp.mongodb.net:27017,ac-3wetz71-shard-00-02.js0j7bp.mongodb.net:27017/?ssl=true&replicaSet=atlas-12i224-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.todo_list
 
 pydantic.json.ENCODERS_BY_TYPE[ObjectId] = str
 
-
-@app.post("/todos")
-async def create_new_todo(request: Request):
-    todo_object = await request.json()
-
-    new_todo = await db["todos"].insert_one(todo_object)
-    created_todo = await db["todos"].find_one({"_id": new_todo.inserted_id})
-
-    return created_todo
+password = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.get("/todos")
-async def get_all_todos():
-    todos = await db["todos"].find().to_list(999)
-    return todos
+class User(BaseModel):
+    Username: str
+    Favourite_Colour: str | None = None
+    Role: str | None = None
 
-@app.get("/todo/{id}")
-async def get_one_todo_by_id(id: str):
-    todo = await db["todos"].find_one({"_id": ObjectId(id)})
-    return todo
+def fake_decode_token(token):
+    return User(
+        Username = token + "Jaden", Favourite_Colour = "Red", Role = "John Doe"
+    )
+
+
